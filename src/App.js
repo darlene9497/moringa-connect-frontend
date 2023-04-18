c40d3203e10581434c8033ee20c951349ab32d57
mukandalee <leonardmukanda254@gmail.com>
2023-04-18 15:29:04 +0300

    did Membership categories and Membership Benefits

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import NavBar from './components/Navbar/NavBar';
 import Footer from './components/Footer/Footer';
 import Demo from './components/Demo';
 import Home from './components/Home/Home'
+import Categories from './components/Membership/Categories';
+import Benefits from './components/Membership/Benefits';
 import History from './components/History/History';
 
 export default function App() {
@@ -15,6 +17,8 @@ export default function App() {
       <Route path='/' element={<Home />}/>
       <Route path='/' element={<Demo />} />
       <Route path='/home/#history' element={<History />} />
+      <Route path='/Membership/#Membership Categories' element={<Categories/>} />
+      <Route path='/Membership/#Membership Benefits' element={<Benefits/>} />
     </Routes>
     <Footer />
     </BrowserRouter>
