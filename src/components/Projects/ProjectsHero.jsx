1cf0d410cfcdd93869ec3294f57ad6648d9e0a1b
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-04-25 09:41:06 +0300

    Fetched projects from server

--- src/components/Projects/ProjectsHero.jsx
+++ src/components/Projects/ProjectsHero.jsx
@@ -4,7 +4,7 @@ export default function ProjectsHero() {
     return (
         <div className="project-hero">
           <div className="overlay-text">
-              <p className='subheading' id="alumni-details" style={{marginTop: '1em'}}>
+              <p className='subheading' id="project-details" style={{marginTop: '1em'}}>
                 Projects
             </p>
             {/* <a class="btn btn-primary" href="/alumni" role="button" style={{textTransform: 'capitalize', color: '#FF5C33', backgroundColor: 'rgba(0, 0, 77, 0.8)',
