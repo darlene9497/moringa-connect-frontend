2c9123c9132d58e835c60fff249e9d6d4d7eeac0
mukandalee <leonardmukanda254@gmail.com>
2023-04-25 09:29:37 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import NavBar from './components/Navbar/NavBar';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home'
-import Categories from './components/Membership/Categories';
-import Benefits from './components/Membership/Benefits';
 import SignUp from './components/signup/signup';
 import LoginForm from './components/login/loginForm';
 import Projects from './components/Projects/Projects';
@@ -32,8 +30,6 @@ export default function App() {
     <NavBar />
     <Routes>
       <Route path='/' element={<Home />}/>
-      <Route path='/Membership/#Membership Categories' element={<Categories/>} />
-      <Route path='/Membership/#Membership Benefits' element={<Benefits/>} />
       <Route path='/signup' element={< SignUp setUserSignedIn={setUserSignedIn} />} />
       <Route path='/login' element={<LoginForm userSignedIn={userSignedIn} setUserSignedIn={setUserSignedIn} />} />
       <Route path='/projects' element={<Projects />} />
--- src/components/Demo.jsx
+++
@@ -1,7 +0,0 @@
-export default function Demo(){
-    return(
-        <>
-            <p style={{paddingTop: '20em', paddingBottom: '20em', textAlign: 'center', fontSize: '1em'}}> This is a demo of the Home page</p>
-        </>
-    )
-}
