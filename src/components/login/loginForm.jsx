f52789aad51103989306005397d45053fed2b097
Darlene Nandabi <117742892+darlene9497@users.noreply.github.com>
2023-05-01 10:59:15 +0300

    Update loginForm.jsx

--- src/components/login/loginForm.jsx
+++ src/components/login/loginForm.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import { useNavigate,Link } from "react-router-dom";
 import './Styles.css'
 
 
