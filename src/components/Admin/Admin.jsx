5bc0a7e6fc9092bd9bfc04236c87e05e6c70dc23
keithkiama <keithkiama2003@gmail.com>
2023-05-02 09:24:33 +0300

    Fixed admin

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardHeader, CardBody } from 'reactbootstrap';
+import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const AdminDashboard = () => {
@@ -9,36 +9,36 @@ const AdminDashboard = () => {
         <div className="col-md-4">
           <Link to="/userlist">
             <Card className="bg-primary text-white">
-              <CardHeader>
+              <Card.Header>
                 <h3 className="mb-0">Users</h3>
-              </CardHeader>
-              <CardBody>
+              </Card.Header>
+              <Card.Body>
                 <p className="mb-0">Manage user accounts and access to the website.</p>
-              </CardBody>
+              </Card.Body>
             </Card>
           </Link>
         </div>
         <div className="col-md-4">
           <Link to="/projectlist">
             <Card className="bg-success text-white">
-              <CardHeader>
+              <Card.Header>
                 <h3 className="mb-0">Projects</h3>
-              </CardHeader>
-              <CardBody>
+              </Card.Header>
+              <Card.Body>
                 <p className="mb-0">Manage and keep track of the status of each project.</p>
-              </CardBody>
+              </Card.Body>
             </Card>
           </Link>
         </div>
         <div className="col-md-4">
           <Link to="/eventlist">
             <Card className="bg-info text-white">
-              <CardHeader>
+              <Card.Header>
                 <h3 className="mb-0">Events</h3>
-              </CardHeader>
-              <CardBody>
+              </Card.Header>
+              <Card.Body>
                 <p className="mb-0">Manage and keep track of event schedules.</p>
-              </CardBody>
+              </Card.Body>
             </Card>
           </Link>
         </div>
