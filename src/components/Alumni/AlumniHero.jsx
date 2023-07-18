532a9f1117b76f0bc00e215b9f16f3b385ddb5f1
iamchacha021 <iamchacha021@gmail.com>
2023-07-18 20:50:39 +0300

    Display the current user's name when they are logged in and the login/ signup button if they are not

--- src/components/Alumni/AlumniHero.jsx
+++ src/components/Alumni/AlumniHero.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import alumniImage from "../../assets/alumni-image.jpeg";
 import './Alumni.css'
+import { AuthContext } from "../AuthContext/AuthContext";
 
 function AlumniHero() {
+  const {user} = useContext(AuthContext)
+  const isLoggedIn = sessionStorage.getItem("jwtToken")
+
     return (
       <div className="hero">
         <img src={alumniImage} alt="Alumni Network" className="hero-image" />
         <div className="hero-content">
           <p>Connect with fellow alumni, find new opportunities, and stay updated with the latest news.</p>
           <div className="buttons">
-            <Link to="/login" className="btn">Log In</Link>
-            <Link to="/signup" className="btn">Sign Up</Link>
+            {
+              isLoggedIn ? (
+                <h3>Hi, {user && user.first_name}</h3>
+              ):(
+                <>
+                  <Link to="/login" className="btn">Log In</Link>
+                  <Link to="/signup" className="btn">Sign Up</Link>
+                </>
+              )
+            }
           </div>
         </div>
       </div>
