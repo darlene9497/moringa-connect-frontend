cbc7ee0eeba76fe099d5b5ddc85fb1263ddf0a50
iamchacha021 <iamchacha021@gmail.com>
2023-07-26 16:41:04 +0300

    Allow admins to be able to upload photos when creating events

--- src/components/Admin/AddEvent.jsx
+++ src/components/Admin/AddEvent.jsx
@@ -10,18 +10,29 @@ export default function AddEvent() {
   const [date, setDate] = useState('');
   const [venue, setVenue] = useState('');
   const [time, setTime] = useState('');
+  const [image, setImage] = useState(null)
   const navigate = useNavigate()
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const eventDetails = {name, description, image_url, date, venue, time}
+    const data = new FormData()
 
+    data.append("[name]", name)
+    data.append("[description]", description)
+    data.append("[image_url]", image_url)
+    data.append("[date]", date)
+    data.append("[venue]", venue)
+    data.append("[time]", time)
+    data.append("[image]", image)
+
+    submitToApi(data)
+
+  }
+
+  function submitToApi(data){
     fetch('/events', {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(eventDetails)
+        body: data
     }).then((res)=> {
         return res.json()
     }).then((event)=> {
@@ -44,7 +55,7 @@ export default function AddEvent() {
               timer: 1500,
             });
             navigate("/allevents");
-          }
+        }
     })
   }
 
@@ -56,28 +67,36 @@ export default function AddEvent() {
             <span>
                 Name
             </span>
-            <input type="text" className='form-control' value={name} onChange={(event) => setName(event.target.value)} />
+            <input type="text" name='name' id='name' className='form-control' value={name} onChange={(event) => setName(event.target.value)} />
         </div>
         
         <div className="form-group my-5">
             <span>
                 Description:
             </span>
-            <textarea className='form-control' value={description} style={{height: "5rem"}} onChange={(event) => setDescription(event.target.value)} />
+            <textarea name='description' id='description' className='form-control' value={description} style={{height: "5rem"}} onChange={(event) => setDescription(event.target.value)} />
         </div>
 
         <div className="form-group my-5">
             <span>
                 image url
             </span>
-            <input type="text" className='form-control' value={image_url} onChange={(event) => setImageUrl(event.target.value)} />
+            <input type="text" name='image_url' id='image_url' className='form-control' value={image_url} onChange={(event) => setImageUrl(event.target.value)} />
+        </div>
+        
+        <div className="form-group my-5">
+            <span>
+                <p>Or Choose</p>
+                Image
+            </span>
+            <input type="file" name='image' id='image' className='form-control' onChange={(event) => setImage(event.target.files[0])} />
         </div>
 
         <div className="form-group my-5">
             <span>
                 Date
             </span>
-            <input type="date" className='form-control' value={date} onChange={(event) => setDate(event.target.value)} />
+            <input type="date" name='date' id='date' className='form-control' value={date} onChange={(event) => setDate(event.target.value)} />
         </div>
 
         
@@ -85,14 +104,14 @@ export default function AddEvent() {
             <span>
                 venue
             </span>
-            <input type="text" className='form-control' value={venue} onChange={(event) => setVenue(event.target.value)} />
+            <input type="text" name='venue' id='venue' className='form-control' value={venue} onChange={(event) => setVenue(event.target.value)} />
         </div>   
 
         <div className="form-group my-5">
             <span>
                 Time
             </span>
-            <input type="time" className='form-control' value={time} onChange={(event) => setTime(event.target.value)} />
+            <input type="time" name='time' id='time' className='form-control' value={time} onChange={(event) => setTime(event.target.value)} />
         </div> 
         
         <button className='btn btn-primary my-2' type="submit">Create Event</button>
